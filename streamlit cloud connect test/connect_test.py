27cacff86b64307173305b10bad0c19fbee6b3c7
Ajiu0519 <178490867@qq.com>
2024-11-27 16:16:01 +0800

    test 4

--- streamlit cloud connect test/connect_test.py
+++ streamlit cloud connect test/connect_test.py
@@ -1,14 +1,4 @@
 import streamlit as st
-#import pymysql
-'''
-# 建立连接
-connection = pymysql.connect(
-    host="192.168.21.32",
-    port=3306,
-    user="remote_user",
-    password="123456",
-    database="mydatabase"
-)'''
 from sqlalchemy import text
 conn = st.connection('mysql', type='sql')
 sql='select * from xunlianying_id'
